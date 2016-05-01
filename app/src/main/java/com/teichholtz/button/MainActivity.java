f92ebf2b8a3e5a607b6b5c94f2e2ed5bd559bacc
teichholtz <davet@teichholtz.com>
2016-05-01 12:31:49 -0400

    First version:  button and reset both work

---
+++ app/src/main/java/com/teichholtz/button/MainActivity.java
@@ -0,0 +1,53 @@
+package com.teichholtz.button;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnClick;
+    Button btnReset;
+
+    TextView txtCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    /* the click button */
+
+    btnClick = (Button)findViewById(R.id.buttonClick);
+
+    /* the Reset button */
+
+    btnReset = (Button)findViewById(R.id.buttonReset);
+
+    txtCount = (TextView)findViewById(R.id.textViewCount);
+
+    btnClick.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v){
+            String countValue=txtCount.getText().toString();
+            int intCountValue = Integer.parseInt(countValue);
+            intCountValue++;
+            txtCount.setText( String.valueOf(intCountValue));
+
+        }
+    });
+        
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                int intCountValue =0;
+                txtCount.setText( String.valueOf(intCountValue));
+            }
+
+        });
+
+    }}
